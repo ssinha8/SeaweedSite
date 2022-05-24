33f72e31d0a6b106ebb3382a46b9d105057f42f4
sbathaee <sbathaee@ucsc.edu>
2022-05-23 18:30:07 -0700

    buttons working. Just needs background now.

--- js/site.js
+++ js/site.js
@@ -1,3 +1,29 @@
 bubbleClick = $("#bubbleClick");
 
-// displays the text bubble for the seaweed bubbl
+// sets the opacity to 100% if 0%, and 0% if 100%
+$("#bubbleClick").click(function() {
+    if ($('#textContainer1').css('opacity') == 0) {
+        $('#textContainer1').fadeTo('fast', 1);
+    }
+    else {
+        $('#textContainer1').fadeTo('fast', 0);
+    }
+});
+
+$("#stemClick").click(function() {
+    if ($('#textContainer2').css('opacity') == 0) {
+        $('#textContainer2').fadeTo('fast', 1);
+    }
+    else {
+        $('#textContainer2').fadeTo('fast', 0);
+    }
+});
+
+$("#holdfastClick").click(function() {
+    if ($('#textContainer3').css('opacity') == 0) {
+        $('#textContainer3').fadeTo('fast', 1);
+    }
+    else {
+        $('#textContainer3').fadeTo('fast', 0);
+    }
+});
