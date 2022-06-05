390165cb8749613e9b489aab51196ac0e8e9acd2
Shloak Sinha <ssinha8@ucsc.edu>
2022-06-05 10:39:57 -0700

    added audio for click

--- js/site.js
+++ js/site.js
@@ -2,6 +2,7 @@ bubbleClick = $("#bubbleClick");
 
 // sets the opacity to 100% if 0%, and 0% if 100%
 $("#bubbleClick").click(function() {
+    document.getElementById("pop").play();
     if ($('#textContainer1').css('opacity') == 0) {
         $('#textContainer1').fadeTo('fast', 1);
     }
@@ -11,6 +12,7 @@ $("#bubbleClick").click(function() {
 });
 
 $("#stemClick").click(function() {
+    document.getElementById("pop").play();
     if ($('#textContainer2').css('opacity') == 0) {
         $('#textContainer2').fadeTo('fast', 1);
     }
@@ -20,6 +22,7 @@ $("#stemClick").click(function() {
 });
 
 $("#holdfastClick").click(function() {
+    document.getElementById("pop").play();
     if ($('#textContainer3').css('opacity') == 0) {
         $('#textContainer3').fadeTo('fast', 1);
     }
