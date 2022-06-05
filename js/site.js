11a7d78556fb8e80710c5105db943acff650aada
Shloak Sinha <ssinha8@ucsc.edu>
2022-06-05 10:58:54 -0700

    Update site.js

--- js/site.js
+++ js/site.js
@@ -1,8 +1,9 @@
-bubbleClick = $("#bubbleClick");
+var sound = document.getElementById("pop");
+sound.volume = 0.1;
 
 // sets the opacity to 100% if 0%, and 0% if 100%
 $("#bubbleClick").click(function() {
-    document.getElementById("pop").play();
+    sound.play();
     if ($('#textContainer1').css('opacity') == 0) {
         $('#textContainer1').fadeTo('fast', 1);
     }
@@ -12,7 +13,7 @@ $("#bubbleClick").click(function() {
 });
 
 $("#stemClick").click(function() {
-    document.getElementById("pop").play();
+    sound.play();
     if ($('#textContainer2').css('opacity') == 0) {
         $('#textContainer2').fadeTo('fast', 1);
     }
@@ -22,7 +23,7 @@ $("#stemClick").click(function() {
 });
 
 $("#holdfastClick").click(function() {
-    document.getElementById("pop").play();
+    sound.play();
     if ($('#textContainer3').css('opacity') == 0) {
         $('#textContainer3').fadeTo('fast', 1);
     }
